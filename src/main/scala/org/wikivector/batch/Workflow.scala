15698e51d4e96556df9ea27919e4a9596062ba5a
crypto5 <cryptofive@gmail.com>
2013-12-13 18:12:53 -0800

    Add comments

--- src/main/scala/org/wikivector/batch/Workflow.scala
+++ src/main/scala/org/wikivector/batch/Workflow.scala
@@ -12,14 +12,34 @@ import org.wikivector.batch.category.CalculateCategoryCounters
 import org.wikivector.batch.category.CalculateLexemeCategoryInfo
 
 object Workflow extends App {
+	// Loads wikipages from xml files to Cassandra
 	LoadWikipages.run
-	TranslateWikiPages.run
+	
+	// Associates english wikipages with pages in other languages
+	// It is not used at the moment, and is disabled to simplify process
+	// TranslateWikiPages.run
+	
+	// Calculates counters for lexemes in wikipages, i.e. number of documents with given lexeme. 
 	CalculateLexemeCounters.run
+	
+	// Deletes rarely used lexemes to make data volume smaller.
 	DeleteRareLexemes.run
+	
+	// Loads categories from xml files to cassandra
 	CreateCategories.run
+	
+	// Creates wikipage category graph and saves it to Cassandra.
 	CreateWikiPageCategoryGraph.run
+	
 	CalculateCategoriesParents.run
+	
+	// Calculates counters for categories, i.e. number of documents within each category.
 	CalculateCategoryCounters.run
+	
+	// Calculates counters of how many times each lexeme occurs in each category.
 	CalculateLexemeCategoryCounters.run
+	
+	// Combine all statistics for categories and lexemes into aggregated MessagePack message.
+	// It allows to make analysis process much faster.
 	CalculateLexemeCategoryInfo.run
 }
